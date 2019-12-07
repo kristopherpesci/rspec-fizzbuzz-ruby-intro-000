83a74043e00ab79d41919faa4c018011c5c62c34
kristopherpesci <kristopher.pesci@gmail.com>
2019-12-07 21:31:40 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,13 +4,4 @@ def fizzbuzz (int)
   if int % 3==0
     "Fizz"
   end
-  if int % 5==0
-    "Buzz"
-  end
-  if int % 5==0 int % 3==0
-    "Fizzbuzz"
-  end
-  if int % 5 > 0 int % 3 > 0
-    "nil"
-  end
 end
